0f9928a04732cf36e7e7420217f92a3494134b5b
Karthick <karthickdo365@gmail.com>
2026-07-28 17:47:14 +0530

    Fix CSS filename case

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/contact.css';
-import "../styles/AboutUs.css";
+
 
 const CONTACT_IMAGE = 'https://images.unsplash.com/photo-1558522195-e1201b090344?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
