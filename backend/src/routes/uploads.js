ba8893d4925ef2252f8b84299202284255392856
Karthick <karthickdo365@gmail.com>
2026-07-29 19:46:02 +0530

    UPDATE PRODUCTS

--- backend/src/routes/uploads.js
+++ backend/src/routes/uploads.js
@@ -5,13 +5,14 @@ import { protect, requireAdmin } from "../middleware/auth.js";
 const router = Router();
 
 router.post("/", protect, requireAdmin, upload.single("image"), (req, res) => {
+    console.log("FILE RECEIVED:", req.file);
   if (!req.file) {
     return res.status(400).json({
       success: false,
       message: "No image uploaded",
     });
   }
-
+ console.log("IMAGE PATH:", req.file.path);   
   return res.status(200).json({
     success: true,
     data: {
